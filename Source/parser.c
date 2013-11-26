eb7aa21a44c5ff7f0d9e7d45d295185dc9aee606
Frank M. Eriksson <git@knarf.se>
2013-11-26 19:14:15 +0100

    Scrapped Ragel-based parser for now
    I will use plain C for the moment as I'm more mentally confortable
    with it.

---
+++ Source/parser.c
@@ -0,0 +1,11 @@
+
+
+/*!	Parse a string representing a YAML stream
+
+	\returns if it sucessfully parsed string
+	\retval 0 means that everything is fine
+*/int yamil_parse_string(void *ast, char *input) {
+	int status = 0;
+	
+	return status;
+}
